b8e360b676de697dd6aa02da02438a89aa2f98ba
Kornel Mrozek <kornel.mrozek@gmail.com>
2020-02-17 01:41:25 +0300

    Removed debug print

--- app/utils/PasswordHasher.scala
+++ app/utils/PasswordHasher.scala
@@ -3,9 +3,5 @@ package utils
 import com.roundeights.hasher.Implicits._
 
 object PasswordHasher {
-  def hashPassword(username: String, password: String): String = {
-    println(s"FOR U: $username and p: $password hash is ${password.salt(username).sha256.hex}")
-    password.salt(username).sha256.hex
-  }
-
+  def hashPassword(username: String, password: String): String = password.salt(username).sha256.hex
 }
